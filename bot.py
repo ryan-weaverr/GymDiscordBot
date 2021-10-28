fd0354ad2bf1b81afef5aff138cc56a16db591ca
ryan-weaverr <82175991+ryan-weaverr@users.noreply.github.com>
2021-10-28 01:01:01 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #import discord library and bot serv
-import os
 import discord
 from botServ import botServ
+import re
 
 #discord client to send commands
 bot = discord.Client()
@@ -10,13 +10,25 @@ bot = discord.Client()
 @bot.event
 async def on_ready():
   print("Bot connected")
-
-#post jpg file when key word is typed
+  
+#event: post jpg file when keyword is typed
 @bot.event
 async def on_message(message):
-  if 'tren' in message.content or 'Tren' in message.content:
-    await message.channel.send("Did someboady say... TREN!?!?")
-    await message.channel.send(file=discord.File('tren.jpg'))
+  # converting message to lowercase to make checking easier 
+  currentMessage = message.content.lower()
+
+  # substituting symbols with spaces 
+  substituedMessage = re.sub(r'[^\w]', ' ', currentMessage)
+  
+  # splitting on spaces and adding to HashSet for constant time complexity on search
+  messageSet = set()
+  messageSet = substituedMessage.split()
+
+  #keyword checks
+  if "tren" in messageSet:
+    if message.author != bot.user:
+      await message.channel.send("Did somebody say... TREN!?!?")
+      await message.channel.send(file=discord.File('tren.jpg'))
 
 botServ()
 TOKEN=("Enter discord token here")
